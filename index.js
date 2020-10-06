88cf682b1ff12f632243e3e121c079350720070a
Kazım Etiksan <kazim@pikselmutfak.com>
2020-10-06 11:04:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/hello', (req, res) => {
+
+    res.send('Hello World')
+})
+
+app.post('/api/name', (req, res) => {
+
+    const body = _.pick(req.body, ['firstName','lastName'])
+
+    console.log(body)
+
+    res.send('Hello '+body.firstName+' '+body.lastName)
+})
+
+app.listen(8080, () => {
+    console.log('app server is running')
+})
